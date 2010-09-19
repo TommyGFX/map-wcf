50f1447c4165fc08f0252470ef8d7be561557c00
torbenbrodt <t.brodt@gmail.com>
2010-09-19 15:03:38 +0000

    add gmap update job

---
+++ optionals/de.gmap.wbb/files/lib/acp/action/UpdateMapAction.class.php
@@ -0,0 +1,68 @@
+<?php
+require_once(WBB_DIR.'lib/acp/action/UpdateCounterAction.class.php');
+require_once(WCF_DIR.'lib/data/gmap/GmapApi.class.php');
+
+/**
+ * Updates the map locations
+ * 
+ * @package     de.gmap.wbb
+ * @author      Torben Brodt
+ * @license	GNU General Public License <http://opensource.org/licenses/gpl-3.0.html>
+ */
+class UpdateMapAction extends UpdateCounterAction {
+	public $action = 'UpdateMap';
+	
+	/**
+	 * @see Action::execute()
+	 */
+	public function execute() {
+		parent::execute();
+		
+		$col = User::getUserOptionID('location');
+		$api = new GmapApi();
+		
+		if(empty($col) || !$api->isActive()) {
+			$this->calcProgress();
+			$this->finish();
+		}
+		
+		$col = "userOption".$col;
+		
+		// count board
+		$sql = "SELECT		COUNT(*) AS count
+			FROM		wcf".WCF_N."_user_option_value
+			WHERE		".$col." != ''";
+		$row = WCF::getDB()->getFirstRow($sql);
+		$count = $row['count'];
+		
+		$i = 0;
+		
+		// update
+		$sql = "SELECT		userID,
+					".$col." AS loc
+			FROM		wcf".WCF_N."_user_option_value
+			ORDER BY	userID";
+		$result = WCF::getDB()->sendQuery($sql, $this->limit, ($this->limit * $this->loop));
+		while ($row = WCF::getDB()->fetchArray($result)) {
+			$point = $api->search($row['loc']);
+			
+			if($point) {
+				$sql = "REPLACE INTO	wcf".WCF_N."_gmap_user
+							(userID, pt)
+					VALUES		(".$row['userID'].", PointFromText('POINT(".$point['lon']." ".$point['lat'].")'))";
+				WCF::getDB()->sendQuery($sql);
+			}
+			
+			$i++;
+		}
+		
+		if ($i == 0) {
+			$this->calcProgress();
+			$this->finish();
+		}
+		
+		$this->calcProgress(($this->limit * $this->loop), $count);
+		$this->nextLoop();
+	}
+}
+?>
