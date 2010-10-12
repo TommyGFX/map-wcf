f49359d192e93636abe8de643e9c58f24b79dcbb
torbenbrodt <t.brodt@gmail.com>
2010-10-12 17:55:51 +0000

    release g-map 2.0.1, with de-informal and use of wcf_n constant

--- optionals/de.gmap.wbb/files/lib/acp/action/UpdateMapAction.class.php
+++ optionals/de.gmap.wbb/files/lib/acp/action/UpdateMapAction.class.php
@@ -4,46 +4,46 @@ require_once(WCF_DIR.'lib/data/gmap/GmapApi.class.php');
 
 /**
  * Updates the map locations
- * 
+ *
  * @package     de.gmap.wbb
  * @author      Torben Brodt
  * @license	GNU General Public License <http://opensource.org/licenses/gpl-3.0.html>
  */
 class UpdateMapAction extends UpdateCounterAction {
 	public $action = 'UpdateMap';
-	
+
 	/**
 	 * @see Action::execute()
 	 */
 	public function execute() {
 		parent::execute();
-		
+
 		$col = User::getUserOptionID('location');
 		$api = new GmapApi();
-		
+
 		if(empty($col) || !$api->isActive()) {
 			$this->calcProgress();
 			$this->finish();
 		}
-		
+
 		$col = "userOption".$col;
-		
+
 		// count board
 		$sql = "SELECT		COUNT(*) AS count
 			FROM		wcf".WCF_N."_user_option_value
 			WHERE		".$col." != ''";
 		$row = WCF::getDB()->getFirstRow($sql);
 		$count = $row['count'];
-		
+
 		$i = 0;
-		
+
 		// cleanup disabled users
-		$sql = 'DELETE FROM	wcf1_gmap_user
-			USING		wcf1_gmap_user
-			LEFT JOIN 	wcf1_user ON wcf1_gmap_user.userID = wcf1_user.userID
-			WHERE		ISNULL(wcf1_user.userID)';
+		$sql = 'DELETE FROM	wcf'.WCF_N.'_gmap_user
+			USING		wcf'.WCF_N.'_gmap_user
+			LEFT JOIN 	wcf'.WCF_N.'_user ON wcf'.WCF_N.'_gmap_user.userID = wcf'.WCF_N.'_user.userID
+			WHERE		ISNULL(wcf'.WCF_N.'_user.userID)';
 		WCF::getDB()->sendQuery($sql);
-		
+
 		// update
 		$sql = "SELECT		userID,
 					".$col." AS loc
@@ -53,22 +53,22 @@ class UpdateMapAction extends UpdateCounterAction {
 		$result = WCF::getDB()->sendQuery($sql, $this->limit, ($this->limit * $this->loop));
 		while ($row = WCF::getDB()->fetchArray($result)) {
 			$point = $api->search($row['loc']);
-			
+
 			if($point) {
 				$sql = "REPLACE INTO	wcf".WCF_N."_gmap_user
 							(userID, pt)
 					VALUES		(".$row['userID'].", PointFromText('POINT(".$point['lon']." ".$point['lat'].")'))";
 				WCF::getDB()->sendQuery($sql);
 			}
-			
+
 			$i++;
 		}
-		
+
 		if ($i == 0) {
 			$this->calcProgress();
 			$this->finish();
 		}
-		
+
 		$this->calcProgress(($this->limit * $this->loop), $count);
 		$this->nextLoop();
 	}
